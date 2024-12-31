e1a04a4a9309fdb4f73118760c04a88480f24c1d
bhnguyen1 <nguyen.brandon316@gmail.com>
2024-12-31 11:06:56 -0600

    authorization token

--- src/common/axiosPrivate.ts
+++ src/common/axiosPrivate.ts
@@ -4,7 +4,7 @@ axios.interceptors.request.use((config) =>{
  
     // Enter generated access token here
     const accessToken =
-      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huX2RvZSIsImV4cCI6MTczNTY2MjUzOCwiaWF0IjoxNzM1NjU4OTM4fQ.dNEc3Hyur30DyEu59ebhfttzl0skI2Gta8lzK_BaSHo';
+      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huX2RvZSIsImV4cCI6MTczNTY2NzcwMywiaWF0IjoxNzM1NjY0MTAzfQ.neVZjRMltdDrIMkseYZ1HCoxT3MmprIyOhX72uCsGYo';
 
     config.headers.Authorization = accessToken;
     return config;
