1a1749f47635f40b233f57c9f9013e34bfafb142
Lawrence Yim <lawrence.yim.010@gmail.com>
2025-01-03 11:40:19 -0500

    Added a wrapper component for routes that can only be visited by unauthenticated guests and will reroute authenticated users.

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,26 @@ import { LoginPage } from './pages/login/LoginPage';
 import { HomePage } from './pages/home/HomePage';
 import NavBarManagement from './components/nav/NavBarManagement';
 import RegisterPage from './pages/register/RegisterPage';
+import { UnauthenticatedRoute } from './components/routes/UnauthenticatedRoute';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-          <NavBarManagement />
-          <Routes>
-            <Route path={ROOT_URL} element={<HomePage />} />
-            <Route path={REGISTER_URL} element={<RegisterPage />} />
-            <Route path={LOGIN_URL} element={<LoginPage />} />
-            {/* Private Route Functionality now works, user must be logged in to visit pages below */}
-            <Route
-              path={HOME_URL}
-              element={<PrivateRoute reactNode={<HomePage />} />}
-            />
-          </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <NavBarManagement />
+                <Routes>
+                    <Route path={ROOT_URL} element={<HomePage />} />
+                    <Route path={REGISTER_URL} element={<UnauthenticatedRoute element={<RegisterPage />} />} />
+                    <Route path={LOGIN_URL} element={<UnauthenticatedRoute element={<LoginPage />} />} />
+                    {/* Private Route Functionality now works, user must be logged in to visit pages below */}
+                    <Route
+                        path={HOME_URL}
+                        element={<PrivateRoute element={<HomePage />} />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
