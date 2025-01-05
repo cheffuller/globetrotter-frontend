c2cb5c897dadd93d92ee4d16f72140b283b3ca33
Lawrence Yim <lawrence.yim.010@gmail.com>
2025-01-05 06:10:00 -0500

    Added follow button.

--- src/pages/userprofile/UserProfileService.ts
+++ src/pages/userprofile/UserProfileService.ts
@@ -1,6 +1,7 @@
 import { API_ROOT_URL } from "../../consts/ApiUrl";
 import { axiosPrivate } from "../../common/axiosPrivate";
 import { UserProfile } from "../../interfaces/UserAccount";
+import { FollowingStatus } from "../../enums/FollowingStatus";
 
 export async function updateProfileRequest(profile: UserProfile): Promise<void> {
     await axiosPrivate.post(`${API_ROOT_URL}users/profile`, profile);
@@ -9,4 +10,11 @@ export async function updateProfileRequest(profile: UserProfile): Promise<void> 
 export async function getProfileByUsernameRequest(username: string): Promise<UserProfile> {
     const response = await axiosPrivate.get(`${API_ROOT_URL}users/${username}/profile`);
     return response.data as UserProfile;
+}
+
+export async function getFollowingStatusRequest(username: string): Promise<FollowingStatus> {
+    const response = await axiosPrivate.get(`${API_ROOT_URL}users/${username}/follow-status`);
+    console.log(response.data);
+    console.log(response.data as FollowingStatus);
+    return response.data as FollowingStatus;
 }
--- src/pages/userprofile/view/UserProfileView.tsx
+++ src/pages/userprofile/view/UserProfileView.tsx
@@ -1,30 +1,58 @@
 import { useState, useEffect } from "react";
 import { UserProfile } from "../../../interfaces/UserAccount";
-import { getProfileByUsernameRequest } from "../UserProfileService";
+import { getFollowingStatusRequest, getProfileByUsernameRequest } from "../UserProfileService";
 import "./UserProfileView.css";
 import { useParams } from "react-router-dom";
+import { ResponseMessage } from "../../../components/response-message/ResponseMessage";
+import { HttpStatusCode } from "axios";
+import { getUsernameFromJwt, removeJwt } from "../../../utils/LocalStorageUtils";
+import { FollowingStatus } from '../../../enums/FollowingStatus';
 
 export function UserProfileView() {
     const { username } = useParams<string>();
     const [bio, setBio] = useState<string>("");
     const [displayName, setDisplayName] = useState<string>("");
     const [isPrivate, setIsPrivate] = useState<boolean>(false);
+    const [isFollowing, setIsFollowing] = useState<FollowingStatus>(FollowingStatus.NotFollowing);
+    const [profileFetched, setProfileFetched] = useState<boolean>(false);
+    const { startWaitingForResponse, stopWaitingAfterFailure, stopWaitingAfterSuccess, getResponseMessage } = ResponseMessage();
 
     useEffect(() => {
         loadProfile();
     }, []);
 
     async function loadProfile() {
-        const profile: UserProfile = await getProfileByUsernameRequest(username as string);
-        setBio(profile.bio);
-        setDisplayName(profile.displayName);
-        setIsPrivate(profile.isPrivate);
+        try {
+            startWaitingForResponse("Waiting for response from the server");
+            const profile: UserProfile = await getProfileByUsernameRequest(username as string);
+            const status: FollowingStatus = await getFollowingStatusRequest(username as string);
+            stopWaitingAfterSuccess("");
+            setBio(profile.bio);
+            setDisplayName(profile.displayName);
+            setIsPrivate(profile.isPrivate);
+            setIsFollowing(status);
+            setProfileFetched(true);
+        } catch (error: any) {
+            switch (error.status) {
+                case HttpStatusCode.NotFound:
+                    stopWaitingAfterFailure("User not found.");
+                    break;
+                default:
+                    stopWaitingAfterFailure("Server is unavailable.");
+            }
+        }
     }
 
-    return <>
-        <div className="userProfile">
-            <h1>{displayName}</h1>
-            <p>{bio}</p>
-        </div>
+return <>
+        {profileFetched ?
+            <div className="userProfile">
+                <h1>{displayName}</h1>
+                <p>{bio}</p>
+                {username != getUsernameFromJwt() ?
+                    <button>{isFollowing}</button> :
+                    <> </>}
+                {getResponseMessage()}
+            </div> :
+            <></>}
     </>;
 }
