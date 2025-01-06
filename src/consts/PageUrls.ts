4fcdbffeef786fce2b839241c3dff9e1bd3bb6e1
Lawrence Yim <lawrence.yim.010@gmail.com>
2025-01-06 16:20:05 -0500

    Refactored nav bar to add a URL for viewing or editing profile and changed display for login and logout button.

--- src/consts/PageUrls.ts
+++ src/consts/PageUrls.ts
@@ -1,7 +1,8 @@
 export const TRAVEL_PLAN_URL = "/travel-plan";
+export const TRAVEL_PLAN_MANAGEMENT_URL = "/travel-plan/management";
 export const REGISTER_URL = "/register";
 export const LOGIN_URL = "/login";
 export const HOME_URL = "/home";
 export const ROOT_URL = "/";
 export const USER_PROFILE_FORM_URL = "/profile/edit";
-export const USER_PROFILE_VIEW_URL = "profile/user/:username";
+export const USER_PROFILE_VIEW_URL = (username: string = ":username") => `profile/user/${username}`;
