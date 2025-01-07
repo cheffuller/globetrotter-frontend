0c120044f3d12170f49926dc59cf9787acdb5b07
Pouyaraj <Pouyaraj74@gmail.com>
2025-01-07 10:13:41 -0600

    commit

--- src/pages/moderate-accessibility/ModerateAccessibility.tsx
+++ src/pages/moderate-accessibility/ModerateAccessibility.tsx
@@ -11,31 +11,31 @@ const ModerateAccessibility = () => {
   const handleBanUser = async () => {
     try {
       await axios.post(`${API_ROOT_URL}moderators/ban/${userId}`);
-      setMessage(`User with ID has been banned.`);
+      setMessage(`User with ID ${userId} has been banned.`);
     } catch (error) {
-      setMessage(`Failed to ban user with ID.`);
+      setMessage(`Failed to ban user with ID ${userId}.`);
     }
   };
 
   const handleUnbanUser = async () => {
     try {
       await axios.delete(`${API_ROOT_URL}moderators/ban/${userId}`);
-      setMessage(`User with ID has been unbanned.`);
+      setMessage(`User with ID ${userId} has been unbanned.`);
     } catch (error) {
-      setMessage(`Failed to unban user with ID.`);
+      setMessage(`Failed to unban user with ID ${userId}.`);
     }
   };
 
   const handleDeleteComment = async () => {
     try {
       await axios.delete(`${API_ROOT_URL}moderators/comments/${commentId}`);
-      setMessage(`Comment with ID has been deleted.`);
+      setMessage(`Comment with ID ${commentId} has been deleted.`);
     } catch (error) {
-      setMessage(`Failed to delete comment with ID.`);
+      setMessage(`Failed to delete comment with ID ${commentId}.`);
     }
-};
+  };
 
-  const handleDeletePlan = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<void> => {
+  const handleDeletePlan = async () => {
     try {
       await axios.delete(`${API_ROOT_URL}plans/${planId}`);
       setMessage(`Plan with ID ${planId} has been deleted.`);
@@ -43,14 +43,38 @@ const ModerateAccessibility = () => {
       setMessage(`Failed to delete plan with ID ${planId}.`);
     }
   };
+
   return (
     <div>
-      <button onClick={handleBanUser}>Ban User</button>
-      <button onClick={handleUnbanUser}>Unban User</button>
-      <br />
-      <button onClick={handleDeleteComment}>Delete Comment</button>
-      <br />
-      <button onClick={handleDeletePlan}>Delete Plan</button>
+      <h2>Moderator Actions</h2>
+      <div>
+        <input
+          type="number"
+          placeholder="User ID"
+          value={userId}
+          onChange={(e) => setUserId(Number(e.target.value))}
+        />
+        <button onClick={handleBanUser}>Ban User</button>
+        <button onClick={handleUnbanUser}>Unban User</button>
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder="Comment ID"
+          value={commentId}
+          onChange={(e) => setCommentId(Number(e.target.value))}
+        />
+        <button onClick={handleDeleteComment}>Delete Comment</button>
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder="Plan ID"
+          value={planId}
+          onChange={(e) => setPlanId(Number(e.target.value))}
+        />
+        <button onClick={handleDeletePlan}>Delete Plan</button>
+      </div>
       {message && <p>{message}</p>}
     </div>
   );
