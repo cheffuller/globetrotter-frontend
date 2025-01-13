f9e85ec508aafa4f2e0c99b4d2420e6b50329565
bhnguyen1 <nguyen.brandon316@gmail.com>
2025-01-13 14:31:39 -0600

    fixed public url

--- src/consts/ApiUrl.ts
+++ src/consts/ApiUrl.ts
@@ -1,4 +1,4 @@
-export const API_ROOT_URL = "http://localhost:8080/"
+export const API_ROOT_URL = "http://54.80.172.33:8080/"
 export const FOLLOWING_STATUS_API = (username: string) => `${API_ROOT_URL}users/${username}/follow-status`;
 export const PROFILE_API = (username: string) => `${API_ROOT_URL}users/${username}/profile`;
 export const UPDATE_PROFILE_API = `${API_ROOT_URL}users/profile`;
